0d2965435d94960ff8d8988f9031d4c260ae0010
Ming-Yuan Yu <myyu@umich.edu>
2018-05-02 23:06:14 -0400

    use matplotlib.patches in raycast2D.py

--- simulation/raycast2D.py
+++ simulation/raycast2D.py
@@ -5,20 +5,18 @@ from multiprocessing import Pool, cpu_count
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import matplotlib.patches as patches
 from shapely.geometry import MultiPolygon, box, Point
 from shapely.affinity import rotate, translate, scale
 from shapely.ops import cascaded_union
 
 
-eps = np.finfo(float).eps
+tol = np.sqrt(np.finfo(float).eps)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--num_rays',
                     help='Number of rays to simulate. (default: 1000)',
                     type=int, default=1000)
-parser.add_argument('--num_draw',
-                    help='Number of rays to draw. (default: 180)',
-                    type=int, default=180)
 parser.add_argument('--range',
                     help='Maximum range of the LiDAR. (default: 5.0)',
                     type=float, default=5.0)
@@ -34,19 +32,20 @@ parser.add_argument('--item_size',
 parser.add_argument('--num_proc',
                     help='Number of threads. If non-positive, use all threads. (default: 0)',
                     type=int, default=0)
+parser.add_argument('--color',
+                    help='Color of the LiDAR. (default: C0)',
+                    type=str, default='C0')
 
 args = parser.parse_args()
 
-
-num_draw = min(args.num_draw, args.num_rays)
-print('Simulating {:d} rays.'.format(args.num_rays))
-print('Drawing {:d} rays.'.format(num_draw))
 
 assert args.num_proc <= cpu_count()
 if args.num_proc <= 0:
     num_proc = cpu_count()
 else:
     num_proc = args.num_proc
+
+num_proc = min(num_proc, args.num_rays)
 
 
 assert args.num_items > 0
@@ -73,28 +72,30 @@ if type(obstacles) is not MultiPolygon:
     obstacles = MultiPolygon([obstacles])
 
 # start (q0) and end (q1) points for all vertices
-q0 = np.vstack([np.asarray(obj.exterior.coords)[:-1, :] for obj in obstacles])
-q1 = np.vstack([np.asarray(obj.exterior.coords)[1:, :] for obj in obstacles])
+q0 = np.vstack([np.asarray(o.exterior.coords)[:-1, :] for o in obstacles])
+q1 = np.vstack([np.asarray(o.exterior.coords)[1:, :] for o in obstacles])
 v = q1 - q0
-
-fig = plt.figure(1, figsize=(6, 6))
-ax = fig.add_subplot(1, 1, 1)
-
-for obj in obstacles:
-    x, y = obj.exterior.coords.xy
-    ax.plot(x, y, 'k', lw=1)
-
-ax.axis('scaled')
-ax.set_xlim(0, args.map_size)
-ax.set_ylim(0, args.map_size)
-
-lines, = ax.plot([], [], lw=0.5)
-center, = ax.plot([], [], 'r+')
 
 # center of the LiDAR
 pt_lidar = np.ones(2) * args.map_size / 2
 theta = np.linspace(0, 2 * np.pi, args.num_rays, endpoint=False)
 u = args.range * np.vstack([np.cos(theta), np.sin(theta)]).T
+
+
+fig = plt.figure(1, figsize=(6, 6))
+ax = fig.add_subplot(1, 1, 1)
+
+for o in obstacles:
+    ax.plot(*o.exterior.coords.xy, 'k', lw=0.5)
+
+center, = ax.plot([], [], '+', color=args.color, ms=16)
+points, = ax.plot([], [], '.', color=args.color, ms=4)
+poly_obsv = patches.Polygon(np.empty([1, 2]), fc=args.color, alpha=0.2)
+patch = ax.add_patch(poly_obsv)
+
+ax.axis('scaled')
+ax.set_xlim(0, args.map_size)
+ax.set_ylim(0, args.map_size)
 
 
 def raycast(pt_lidar_, u_):
@@ -113,7 +114,7 @@ def raycast(pt_lidar_, u_):
 
     uxv = np.vstack([np.cross(aa, v) for aa in u_])
 
-    uxv[uxv == 0] = np.sqrt(eps)
+    uxv[uxv == 0] = tol
 
     t = dxv / uxv
     s = dxu / uxv
@@ -132,14 +133,12 @@ fig.canvas.mpl_connect('motion_notify_event', on_move)
 
 pool = Pool(num_proc)
 input_args = []
-num_pt_per_proc = np.ceil(args.num_rays / num_proc).astype(int)
+num_pt_per_proc = args.num_rays // num_proc
 for k in range(num_proc):
     aa = u[(k * num_pt_per_proc):((k + 1) * num_pt_per_proc)]
     input_args.append((pt_lidar, aa))
 
-step = max(np.ceil(args.num_rays / num_draw).astype(int), 1)
-pt_x = np.nan * np.empty([args.num_rays, 3])
-pt_y = np.nan * np.empty([args.num_rays, 3])
+
 timestamps = np.empty(10)
 
 
@@ -147,26 +146,25 @@ def update(i):
     timestamps[1:] = timestamps[:-1]
     timestamps[0] = time.time()
 
-    points = np.vstack(pool.starmap(raycast, input_args))
-
-    pt_x[:, 0] = pt_lidar[0]
-    pt_x[:, 1] = points[:, 0]
-
-    pt_y[:, 0] = pt_lidar[1]
-    pt_y[:, 1] = points[:, 1]
-
-    lines.set_xdata(pt_x[::step, :])
-    lines.set_ydata(pt_y[::step, :])
+    xy = np.vstack(pool.starmap(raycast, input_args))
 
     center.set_xdata(pt_lidar[0])
     center.set_ydata(pt_lidar[1])
+
+    patch.set_xy(xy)
+
+    dist = np.linalg.norm(xy - pt_lidar[None, :], axis=-1)
+    idx = dist < (1 - tol) * args.range
+
+    points.set_xdata(xy[idx, 0])
+    points.set_ydata(xy[idx, 1])
 
     dt = np.mean(timestamps[:-1] - timestamps[1:])
 
     if i >= timestamps.size:
         print('average fps = {:.4f}'.format(1 / dt), end='\r')
 
-    return [lines, center]
+    return [patch, center, points]
 
 
 ani = FuncAnimation(fig, update, interval=0, blit=True)
