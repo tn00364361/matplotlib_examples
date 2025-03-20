5361f295a317311803ad11d4c05e0bc959de7e2f
Ming-Yuan Yu <3705281+tn00364361@users.noreply.github.com>
2025-03-19 23:44:45 -0500

    rewrite barnsley fern

--- geometry/barnsley_fern.py
+++ geometry/barnsley_fern.py
@@ -1,70 +1,87 @@
 #! /usr/bin/python3
 import argparse
+from functools import partial
+import sys
 from multiprocessing import Pool, cpu_count
 import numpy as np
 import numpy.random as random
 import matplotlib.pyplot as plt
 
 
-'''
-Reference:
-    https://en.wikipedia.org/wiki/Barnsley_fern
-'''
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--num_pt', '-n', type=int, default=1000000,
-                    help='Number of points. (default: 1000000)')
-parser.add_argument('--resolution', '-r', type=float, default=0.02,
-                    help='Resolution of the grids. (default: 0.02)')
-
-args = parser.parse_args()
+"""
+Reference:  https://en.wikipedia.org/wiki/Barnsley_fern
+"""
 
 
-coeffs = np.array([
-    [0, 0, 0, 0.16, 0, 0],
-    [0.85, 0.04, -0.04, 0.85, 0, 1.60],
-    [0.20, -0.26, 0.23, 0.22, 0, 1.60],
-    [-0.15, 0.28, 0.26, 0.24, 0, 0.44]
-])
-prob = np.array([0.01, 0.85, 0.07, 0.07])
+coeffs = np.array(
+    [
+        [0, 0, 0, 0.16, 0, 0],
+        [0.85, 0.04, -0.04, 0.85, 0, 1.60],
+        [0.20, -0.26, 0.23, 0.22, 0, 1.60],
+        [-0.15, 0.28, 0.26, 0.24, 0, 0.44],
+    ],
+    dtype=np.float32,
+)
+prob = np.array([0.01, 0.85, 0.07, 0.07], dtype=np.float32)
 
 
-def f(xy):
-    idx = random.choice(4, p=prob)
-    A = coeffs[idx, :4].reshape([2, 2])
-    b = coeffs[idx, 4:]
-    return np.dot(A, xy) + b
-
-
-num_proc = cpu_count()
-num_iter = np.round(args.num_pt / num_proc).astype(int) + 1
-
-
-def iterate(seed):
-    random.seed(seed)
-
-    points = np.zeros([2, num_iter])
-    for k in range(1, num_iter):
-        points[:, k] = f(points[:, k - 1])
+def iterate_one_point(seed: int, num_iterations: int):
+    rng = np.random.default_rng(seed)
+    indices = rng.choice(4, size=num_iterations, p=prob)
+    points = np.empty([2, num_iterations + 1], dtype=np.float32)
+    points[:, 0] = rng.random(2)
+    for k in range(num_iterations):
+        A = coeffs[indices[k], :4].reshape([2, 2])
+        b = coeffs[indices[k], 4:]
+        points[:, k + 1] = A @ points[:, k] + b
 
     return points[:, 1:]
 
 
-with Pool(num_proc) as pool:
-    seeds = random.randint(2**32 - 1, size=num_proc)
-    xy = np.hstack(pool.map(iterate, seeds))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--num_points",
+        "-n",
+        type=int,
+        default=1_000_000,
+        help="Number of points. (default: 1000000)",
+    )
+    parser.add_argument(
+        "--resolution",
+        "-r",
+        type=float,
+        default=0.02,
+        help="Resolution of the grids. (default: 0.02)",
+    )
 
-x_bin = np.arange(-3, 3, args.resolution)
-y_bin = np.arange(-0.5, 10.5, args.resolution)
+    args = parser.parse_args()
+    num_proc = cpu_count()
+    num_iterations = np.round(args.num_points / num_proc).astype(int)
 
-H = np.histogram2d(xy[0, :], xy[1, :], bins=[x_bin, y_bin])[0]
+    with Pool(num_proc) as pool:
+        seeds = random.randint(2**32 - 1, size=num_proc)
+        xy = np.hstack(
+            pool.map(partial(iterate_one_point, num_iterations=num_iterations), seeds)
+        )
 
-fig = plt.figure(1, figsize=(5, 8))
-ax = fig.add_subplot(1, 1, 1)
+    x_bin = np.arange(-3, 3, args.resolution)
+    y_bin = np.arange(-0.5, 10.5, args.resolution)
 
-ax.imshow(np.log1p(H).T,
-          origin='lower',
-          extent=(x_bin.min(), x_bin.max(), y_bin.min(), y_bin.max()))
+    H, _, _ = np.histogram2d(xy[0, :], xy[1, :], bins=[x_bin, y_bin])
 
-fig.tight_layout()
-plt.show()
+    fig = plt.figure(1, figsize=(5, 8))
+    ax = fig.add_subplot(1, 1, 1)
+
+    ax.imshow(
+        np.log1p(H).T,
+        origin="lower",
+        extent=(x_bin.min(), x_bin.max(), y_bin.min(), y_bin.max()),
+    )
+
+    fig.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
